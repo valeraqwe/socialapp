c851f0a1ca3a373d2d21d7346e61568962180da0
valeraqwe <valera.komar.1999@gmail.com>
2023-05-27 09:39:43 +0300

    modemon ext and yaml docs

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const format = require('date-format');
 const app = express();
 
+// swagger docs related
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
