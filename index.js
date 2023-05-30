7862d1386ebcb894393a10bd76028e89ae2957bd
valeraqwe <valera.komar.1999@gmail.com>
2023-05-30 10:19:41 +0300

    swagger docs

--- index.js
+++ index.js
@@ -1,62 +1,77 @@
-const express = require('express');
-const format = require('date-format');
+const express = require("express");
+
 const app = express();
 
-// swagger docs related
-const swaggerUi = require('swagger-ui-express');
-const fs = require("fs")
-const YAML = require('yaml')
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+const fileUpload = require("express-fileupload");
 
-const file  = fs.readFileSync('./swagger.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(fileUpload());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+let courses = [
+    {
+        id: "11",
+        name: "Learn Reactjs",
+        price: 299,
+    },
+    {
+        id: "22",
+        name: "Learn Angular",
+        price: 399,
+    },
+    {
+        id: "33",
+        name: "Learn Django",
+        price: 499,
+    },
+];
 
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-    res.status(200).send("<h1>Hello from Valeraaa 2024 yesssss!</h1>");
+app.get("/", (req, res) => {
+    res.send("hello from lco");
 });
 
-app.get("/api/v1/facebook", (req, res) => {
-    const facebookData = {
-        username: "valerafb",
-        followers: 33,
-        follows: 11,
-        date: format.asString("yyyy-MM-dd.", new Date())
-    };
-
-    res.status(200).json(facebookData);
+app.get("/api/v1/lco", (req, res) => {
+    res.send("hello from lco docs");
 });
 
-app.get("/api/v1/instagram", (req, res) => {
-    const instagramData = {
-        username: "valerainsta",
-        followers: 239,
-        follows: 118,
-        date: format.asString("yyyy-MM-dd.", new Date())
-    };
-
-    res.status(200).json(instagramData);
+app.get("/api/v1/lcoobject", (req, res) => {
+    res.send({ id: "55", name: "Learn Backend", price: 999 });
 });
 
-app.get("/api/v1/linkedin", (req, res) => {
-    const linkedinData = {
-        username: "valeralk",
-        followers: 3,
-        follows: 3,
-        date: format.asString("yyyy-MM-dd.", new Date())
-    };
-
-    res.status(200).json(linkedinData);
+app.get("/api/v1/courses", (req, res) => {
+    res.send(courses);
 });
 
-app.get("/api/v1/:token", (req, res) => {
-    const token = req.params.token;
-    console.log(token);
-    res.status(200).json({ param: token });
+app.get("/api/v1/mycourse/:courseId", (req, res) => {
+    const myCourse = courses.find((course) => course.id === req.params.courseId);
+    res.send(myCourse);
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+app.post("/api/v1/addCourse", (req, res) => {
+    console.log(req.body);
+    courses.push(req.body);
+    res.send(true);
 });
+
+app.get("/api/v1/coursequery", (req, res) => {
+    let location = req.query.location;
+    let device = req.query.device;
+
+    res.send({ location, device });
+});
+
+app.post("/api/v1/courseupload", (req, res) => {
+    console.log(req.headers);
+    const file = req.files.file;
+    console.log(file);
+    let path = __dirname + "/images/" + Date.now() + ".jpg";
+
+    file.mv(path, (err) => {
+        res.send(true);
+    });
+});
+
+app.listen(4000, () => console.log(`Server is running at port 4000...`));
